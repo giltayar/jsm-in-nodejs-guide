dfa8cad0706acc1199c8f88c370e669691e18cc7
Gil Tayar <gil@tayar.org>
2021-01-29 12:21:54 +0200

    made simplest templates test be something
    [bilt-with-bilt]

--- 01-simplest-mjs/test/tryout.mjs
+++ 01-simplest-mjs/test/tryout.mjs
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import {banner} from '../src/main.mjs'
 
-assert.strict.match(banner, /42/)
+assert.strict.match(banner, /The answer is.*42/)
 
 console.log(banner)
--- 02-simplest-js/test/tryout.js
+++ 02-simplest-js/test/tryout.js
@@ -1,3 +1,6 @@
+import assert from 'assert'
 import {banner} from '../src/main.js'
+
+assert.strict.match(banner, /The answer is.*42/)
 
 console.log(banner)
